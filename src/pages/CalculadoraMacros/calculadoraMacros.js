662bc9342ae7ef09bc988603b57956d4bf71b4f9
imano <imanolquinber26@gmail.com>
2024-03-09 16:42:26 +0000

    Backend y funcionalidad de la calculadora de Macros

--- src/pages/CalculadoraMacros/calculadoraMacros.js
+++ src/pages/CalculadoraMacros/calculadoraMacros.js
@@ -3,13 +3,22 @@ import './calculadoraMacros.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFire, faCheese, faBreadSlice, faFish, faPlay } from '@fortawesome/free-solid-svg-icons'
 import Header from '../../components/Header/header';
-
 function CalculadoraMacros(){
-    const  [altura, setAltura] = useState("");
-    const  [edad, setEdad] = useState("");
-    const  [peso, setPeso] = useState("");
-    const [resultadoIMC, setIMC] = useState("");
+
+    // Lectura de datos: sistema, altura, edad, peso, genero, actividad y meta
     const [sistema, setSistema] = useState("metrico");
+    const [altura, setAltura] = useState("");
+    const [edad, setEdad] = useState("");
+    const [peso, setPeso] = useState("");
+    const [genero, setGenero] = useState("hombre");
+    const [actividad, setActividad] = useState("sedentario");
+    const [meta, setMeta] = useState("perder-peso");
+
+    // Resultados de calorías, proteínas, carbohidratos y grasas
+    const [resultadoCalorias, setCalorias] = useState("");
+    const [resultadoProteinas, setProteinas] = useState("");
+    const [resultadoCarbohidratos, setCarbohidratos] = useState("");
+    const [resultadoGrasas, setGrasas] = useState("");
 
     let checks = document.querySelectorAll("input[type=checkbox]");
     checks.forEach(check => check.addEventListener("change", function(e){
@@ -34,27 +43,81 @@ function CalculadoraMacros(){
         setSistema(event.target.value);
     }
 
-    const IMC = () => {
-        if (!altura || !peso){
+    const seleccionGenero = (event) => {
+        setGenero(event.target.value);
+    }
+
+    const seleccionActividad = (event) => {
+        setActividad(event.target.value);
+    }
+
+    const seleccionMeta = (event) => {
+        setMeta(event.target.value);
+    }
+
+    const calcularMacros = () => {
+        if (!altura || !peso || !edad) {
             alert('Por favor, complete todos los campos');
             return;
         }
 
-        if (sistema === "anglosajon"){
-            const alturaUser = parseFloat(altura.replace(",", "."));
-            const pesoUser = parseFloat(peso.replace(",", "."));
+        const factorSedentario = 1.2;
+        const factorModerado = 1.375;
+        const factorIntenso = 1.60;
 
-            const resultado = (pesoUser * 0.453592)/((alturaUser * 0.3048) ** 2);
-            setIMC(resultado.toFixed(2));
+        const alturaUser = parseFloat(altura);
+        const pesoUser = parseFloat(peso);
+
+        let objetivoCalorias;
+        switch (meta) {
+            case "ganar-peso":
+                objetivoCalorias = 500; // Añadir 500 calorías por día para ganar peso
+                break;
+            case "perder-peso":
+                objetivoCalorias = -500; // Restar 500 calorías por día para perder peso
+                break;
+            case "mantener-peso":
+            default:
+                objetivoCalorias = 0; // Mantener el mismo número de calorías
+                break;
         }
-        else{
-            const alturaUser = parseFloat(altura);
-            const pesoUser = parseFloat(peso.replace(",", "."));
     
-            const resultado = pesoUser/((alturaUser / 100) ** 2);
-            setIMC(resultado.toFixed(2));
+        let TMB; // Tasa metabólica basal
+        let actividadFactor; // Factor de actividad
+    
+        // Calcular la TMB según el género
+        if (genero === "hombre") {
+            TMB = 10 * pesoUser + 6.25 * alturaUser - 5 * edad + 5;
+        } else {
+            TMB = 10 * pesoUser + 6.25 * alturaUser - 5 * edad - 161;
         }
+    
+        // Calcular el factor de actividad
+        if (actividad === "sedentario") {
+            actividadFactor = factorSedentario;
+        } else if (actividad === "moderado") {
+            actividadFactor = factorModerado;
+        } else {
+            actividadFactor = factorIntenso;
+        }
+    
+        // Calcular las calorías necesarias por día
+        const calorias = TMB * actividadFactor + objetivoCalorias;
+        setCalorias(calorias.toFixed(2));
+    
+        // Calcular las proteínas necesarias
+        const proteinas = pesoUser * 2.2; // En gramos
+        setProteinas(proteinas.toFixed(2));
+    
+        // Calcular las grasas necesarias
+        const grasas = (calorias * 0.25) / 9; // El 25% de las calorías diarias proviene de grasas
+        setGrasas(grasas.toFixed(2));
+    
+        // Calcular los carbohidratos necesarios
+        const carbohidratos = (calorias - (proteinas * 4) - (grasas * 9)) / 4; // Los carbohidratos aportan 4 calorías por gramo
+        setCarbohidratos(carbohidratos.toFixed(2));
     };
+
 
     return (
         <div className="contenedor-macros">
@@ -86,7 +149,7 @@ function CalculadoraMacros(){
                     <div className="introducir-genero">
                         <form className="genero">
                             <label className="info-genero">{"Género:"}</label>
-                            <select id="info-genero" className="info-genero">
+                            <select id="info-genero" className="info-genero" value={genero} onChange={seleccionGenero}>
                                 <option value="hombre">Hombre</option> 
                                 <option value="mujer">Mujer</option>
                             </select>
@@ -110,7 +173,7 @@ function CalculadoraMacros(){
                     <div className="introducir-actividad">
                         <form className="actividad">
                             <label htmlFor="actividad-fisica">Actividad física actual:</label>
-                            <select id="actividad-fisica" className="input-actividad">
+                            <select id="actividad-fisica" className="input-actividad" value={actividad} onChange={seleccionActividad}>
                                 <option value="sedentario">Sedentario</option> 
                                 <option value="moderado">Moderado</option>
                                 <option value="intensa">Intensa</option>
@@ -121,35 +184,35 @@ function CalculadoraMacros(){
                     <div className="introducir-meta">
                         <label className="info-meta">{"Meta:"}</label>
                         <ul>
-                            <li><input type="radio" id="perder-peso" name="meta" value="perder-peso" /><label htmlFor="perder-peso">Perder peso</label></li>
-                            <li><input type="radio" id="mantener" name="meta" value="mantener" /><label htmlFor="mantener">Mantener</label></li>
-                            <li><input type="radio" id="ganar-peso" name="meta" value="ganar-peso" /><label htmlFor="ganar-peso">Ganar peso</label></li>
+                            <li><input type="radio" id="perder-peso" name="meta" value="perder-peso" checked={meta === "perder-peso"} onChange={seleccionMeta}/><label htmlFor="perder-peso">Perder peso</label></li>
+                            <li><input type="radio" id="mantener" name="meta" value="mantener" checked={meta === "mantener"} onChange={seleccionMeta}/><label htmlFor="mantener">Mantener</label></li>
+                            <li><input type="radio" id="ganar-peso" name="meta" value="ganar-peso" checked={meta === "ganar-peso"} onChange={seleccionMeta}/><label htmlFor="ganar-peso">Ganar peso</label></li>
                         </ul>
                     </div>
                 </div>
-                <button className="calcularMacros" onClick={IMC}>Calcular</button>
+                <button className="calcularMacros" onClick={calcularMacros}>Calcular</button>
                 <div className="resultado-calculoMacros">
                     <div className="resultado-label">Los macronutrientes necesarios son:</div>
                     <div className="contenedor-recuadros">
                         <div className="recuadro">
                             <FontAwesomeIcon icon={faFire} className="icon-calorias" />
                             <h3>Calorías</h3>
-                            <p className="info" id="calorias-info">x calorias por día</p>
+                            <p className="info" id="calorias-info">{resultadoCalorias} calorias por día</p>
                         </div>
                         <div className="recuadro">
                             <FontAwesomeIcon icon={faBreadSlice} className="icon-carbos" />
                             <h3>Carbohidratos</h3>
-                            <p className="info" id="carbohidratos-info">x gramos por día</p>
+                            <p className="info" id="carbohidratos-info">{resultadoCarbohidratos} gramos por día</p>
                         </div>
                         <div className="recuadro">
                             <FontAwesomeIcon icon={faFish} className="icon-proteinas" />
                             <h3>Proteínas</h3>
-                            <p className="info" id="proteinas-info">x gramos por día</p>
+                            <p className="info" id="proteinas-info">{resultadoProteinas} gramos por día</p>
                         </div>
                         <div className="recuadro">
                             <FontAwesomeIcon icon={faCheese} className="icon-grasas" />
                             <h3>Grasas</h3>
-                            <p className="info" id="grasas-info">x gramos por día</p>
+                            <p className="info" id="grasas-info">{resultadoGrasas} gramos por día</p>
                         </div>
                     </div>
                 </div>
